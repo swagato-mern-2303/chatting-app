6d01c29a16255b2782bf19e7223a715829182f4a
swagato-mern-2303 <swagatomern2303@gmail.com>
2023-10-09 19:54:01 +0600

    registration-page-responsive

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -8,38 +8,40 @@ function Registration() {
   return (
     <div className="font-nunito">
       <div className="flex items-center">
-        <div className="flex w-1/2 flex-col items-end">
-          <div className="mr-[70px]">
-            <h1 className="text-primary-color-400 mb-[13px] text-[34px] font-bold">
-              Get started with easily register
-            </h1>
-            <p className="mb-[62px] text-[20.6px] opacity-60">
-              Free register and you can enjoy it
-            </p>
-            <form className="flex w-[370px] flex-col gap-y-[57px]">
-              {registrationInputData.map((item, index) => (
-                <RegistrationInput
-                  key={index}
-                  type={item.type}
-                  label={item.label}
-                  id={item.id}
-                />
-              ))}
-              <button className="bg-primary-accent rounded-[86px] py-5 text-[20.64px] font-semibold text-white duration-200 hover:bg-blue-800">
-                Sign up
-              </button>
-            </form>
-            <div className="font-open-sans mt-[20px] w-[370px] text-center">
-              <p className="text-primary-color-400 text-[13.338px]">
-                Already have an account ?{" "}
-                <a className="text-secondary-accent font-bold" href="">
-                  Sign In
-                </a>
+        <div className="w-full xl:w-1/2">
+          <div className="flex min-h-screen flex-col items-center justify-center bg-registration-page bg-cover bg-center bg-no-repeat p-5 xl:items-end xl:bg-none">
+            <div className="rounded-lg bg-white p-5 xl:mr-[70px] xl:p-0">
+              <h1 className="mb-[13px] text-[34px] font-bold leading-tight text-primary-color-400">
+                Get started with easily register
+              </h1>
+              <p className="mb-8 text-[20.6px] opacity-60 md:mb-[62px]">
+                Free register and you can enjoy it
               </p>
+              <form className="mx-auto flex max-w-[370px] flex-col gap-y-8 md:gap-y-[57px] xl:mx-0">
+                {registrationInputData.map((item, index) => (
+                  <RegistrationInput
+                    key={index}
+                    type={item.type}
+                    label={item.label}
+                    id={item.id}
+                  />
+                ))}
+                <button className="rounded-[86px] bg-primary-accent py-5 text-[20.64px] font-semibold text-white duration-200 hover:bg-blue-800">
+                  Sign up
+                </button>
+              </form>
+              <div className="mx-auto mt-[20px] max-w-[370px] text-center font-open-sans xl:mx-0">
+                <p className="text-[13.338px] text-primary-color-400">
+                  Already have an account ?{" "}
+                  <a className="font-bold text-secondary-accent" href="">
+                    Sign In
+                  </a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="bg-registration-page h-screen w-1/2 bg-cover bg-center bg-no-repeat"></div>
+        <div className="hidden h-screen w-1/2 bg-registration-page bg-cover bg-center bg-no-repeat xl:block"></div>
       </div>
     </div>
   );
@@ -50,13 +52,13 @@ function RegistrationInput({ type, label, id }) {
     <>
       <div className="relative">
         <label
-          className="text-primary-color-400/70 absolute left-[29px] top-0 w-[155px] -translate-y-1/2 bg-white py-3 text-center text-[13.76px] font-semibold"
+          className="absolute left-[29px] top-0 w-[155px] -translate-y-1/2 bg-white py-3 text-center text-[13.76px] font-semibold text-primary-color-400/70"
           htmlFor={id}
         >
           {label}
         </label>
         <input
-          className="border-primary-color-400/30 text-primary-color-400 w-full rounded-[8.6px] border-2 p-[27px] text-[20.641px] font-semibold focus:outline-none"
+          className="w-full rounded-[8.6px] border-2 border-primary-color-400/30 p-4 text-[20.641px] font-semibold text-primary-color-400 focus:outline-none md:p-[27px]"
           id={id}
           type={type}
         />
